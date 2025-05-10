f6cff48f9c81e2155595cf8ae812269932b30b97
OneHoWon <flash0221@ajou.ac.kr>
2025-05-11 01:11:52 +0900

    feat: 스타일 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
+// src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
+// 전역 스타일(꼭 이 파일을 불러와야 Tailwind가 적용됩니다)
+import './index.css'
+
 import App from './App'
+
+// (기존에 App.css 같은 게 있다면 모두 지우셔도 무방합니다)
+// import './App.css'
+
 import { TableProvider } from './contexts/TableContext'
 import { CartProvider }  from './contexts/CartContext'
-
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -17,5 +23,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </BrowserRouter>
       </CartProvider>
     </TableProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
