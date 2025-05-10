e24762384387cc89565798649d870b56a2621795
OneHoWon <flash0221@ajou.ac.kr>
2025-05-11 01:15:45 +0900

    feat: 스타일 수정

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,6 @@
-// 프로젝트 루트/postcss.config.cjs
+// postcss.config.cjs
 module.exports = {
   plugins: {
-    // ⬇️ 기존 tailwindcss 대신에 @tailwindcss/postcss
     '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
