6868f0d7807836723414efe0072415d13288d9b9
onehowon <flash0221@naver.com>
2025-05-12 16:59:24 +0900

    feat: 주문 상세 페이지

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,12 @@ import App from './App'
 import './index.css'
 import { TableProvider } from '@/contexts/TableContext'
 import { CartProvider }  from '@/contexts/CartContext'
+import adminApi from '@/api'
+
+const savedToken = localStorage.getItem('accessToken')
+if (savedToken) {
+  adminApi.defaults.headers.common['Authorization'] = `Bearer ${savedToken}`
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
